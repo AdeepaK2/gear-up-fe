c5b2f1255ef99159c1fb1cd8a4dc7a6fe862793a
AdeepaK2 <adeepashashintha@gmail.com>
2025-11-07 10:02:18 +0530

    Remove runtime configuration file for local development

--- public/config.js
+++
@@ -1,5 +0,0 @@
-// Runtime configuration - will be replaced by Docker entrypoint
-// This is the DEFAULT for local development
-window.__RUNTIME_CONFIG__ = {
-  NEXT_PUBLIC_API_URL: 'http://localhost:8080/api/v1'
-};
